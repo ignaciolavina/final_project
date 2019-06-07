2fd42eaff1ed50f3e7460012befa7f41e57fb08e
lavinaIgnacio <ucone92@gmail.com>
2019-06-07 12:36:14 -0700

    now auth user with request.vars

--- web2py/applications/final_project/controllers/api.py
+++ web2py/applications/final_project/controllers/api.py
@@ -111,14 +111,14 @@ def save_profile():
     user = request.vars.user
     print("requests" , request.vars)
     print("requests userz" , request.vars.user)
-    # print (user.last_name)
+    print (user.last_name)
     auth.user.last_name = request.vars.string
     # auth.user.first_name = request.vars.user.first_name 
     print ("auth user", auth.user.last_name)
     
     query = db(db.auth_user.id == auth.user_id).select().first()
     # db(db.auth_user.id == auth.user_id)
-    query.update_record(last_name="leruki")
+    query.update_record(last_name=request.vars.string)
     print ("query:", query.last_name)
     
     return response.json(dict())
